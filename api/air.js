ec8ef860a716ec434c17142a2679ffaf1c683781
son2k104 <nguyenngotrungson04012010@gmail.com>
2026-02-28 17:42:11 +0700

    debug

--- api/air.js
+++ api/air.js
@@ -1,9 +1,9 @@
 export default async function handler(req, res) {
   const { lat, lon } = req.query;
-  const key = process.env.OPENWEATHER_KEY;
+  const API_KEY = process.env.OPENWEATHER_KEY;
 
   const response = await fetch(
-    `https://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${key}`
+    `https://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${API_KEY}`
   );
 
   const data = await response.json();
